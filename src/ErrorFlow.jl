dfa8973371bbfacfc5b2c4106b05e151417bd9bc
Jan Weidner <jw3126@gmail.com>
2020-12-03 16:04:01 +0100

    init

--- src/ErrorFlow.jl
+++ src/ErrorFlow.jl
@@ -1,5 +1,79 @@
 module ErrorFlow
 
-# Write your package code here.
+export @catch
+export @chain
+export Error
+
+struct Error
+    value
+    hints
+    stacktrace
+end
+
+function Base.showerror(io::IO, err::Error)
+    println(io, "Error:")
+    Base.showerror(io, err.value)
+    println(io)
+    println(io)
+    println(io, "Hints:")
+    for (i, hint) in pairs(err.hints)
+        print(io, i, ") ")
+        println(io, hint)
+    end
+    Base.show_backtrace(
+        io, err.stacktrace
+    )
+end
+
+function append_hints(err::Error, hints)
+    Error(err.value, [err.hints; hints], err.stacktrace)
+end
+
+macro chain(code, err)
+    chain_macro(code, err)
+end
+
+macro var"catch"(code, hint=nothing)
+    code = esc(code)
+    if (hint === nothing)
+        hints = []
+    else
+        hints = esc(:([$hint]))
+    end
+    quote
+        try
+            $code
+        catch err
+            st = $stacktrace($catch_backtrace())
+            $Error(err, $hints, st)
+        end
+    end
+end
+
+function chain_macro(code, hint=nothing)
+    code = esc(code)
+    if (hint === nothing)
+        hints = []
+    else
+        hints = esc(:([$hint]))
+    end
+    quote
+        result = try
+            $code
+        catch err
+            st = $stacktrace($catch_backtrace())
+            ce = $Error(err, $hints, st)
+            return ce
+        end
+        if result isa $Error
+            return $append_hints(result, $hints)
+        elseif result isa Exception
+            st = $stacktrace($catch_backtrace())
+            return $Error(result, $hints, st)
+        else
+            result
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,37 @@
-using ErrorFlow
 using Test
+using ErrorFlow
 
-@testset "ErrorFlow.jl" begin
-    # Write your tests here.
+@testset "@catch" begin
+    err = @catch error("oh no")
+    @test err.value == ErrorException("oh no")
+    @test err.hints == []
+
+    err = @catch error("oh no") "bad"
+    @test err.value == ErrorException("oh no")
+    @test err.hints == ["bad"]
+end
+
+
+@testset "@chain example" begin
+    msg_fail_load_config = "Failed to load config"
+    msg_fail_parse_config = "Failed to parse config"
+
+    app(succeed) = @chain load_config(succeed) msg_fail_load_config
+    load_config(succeed) = @chain parse_line157(succeed) msg_fail_parse_config
+    function parse_line157(succeed)
+        if succeed
+            return 42
+        else
+            error("Cannot parse line 157")
+        end
+    end
+
+    @test app(true) == 42
+    @inferred Union{Int, Error} app(true)
+    err = app(false)
+    @test err.value == ErrorException("Cannot parse line 157")
+    s = sprint(showerror, err)
+    @test occursin(msg_fail_load_config, s)
+    @test occursin(msg_fail_parse_config, s)
+    @test occursin("parse_line157", s)
 end
